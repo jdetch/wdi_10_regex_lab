7ad1ccc20bcf60c0d9c2c0b01a4b7086b1393959
Jodi Detch <jodidetch@gmail.com>
2014-08-14 10:03:42 -0400

    Has regex for valid IP addresses

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
 require_relative 'source'
 require 'pry'
 
-def valid_ip_addresses
+def valid_ip_addresses(text)
+  text.scan(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
+  # braces enclose ranges
+  # need backslashes to escape dots because . stands for any single character
+  # Another solution (?:\d{1,3}\.){3}\d{1,3}
 end
 
-def valid_mit_ip_addresses
+def valid_mit_ip_addresses()
 end
 
 def non_mit_ip_addresses
@@ -13,7 +17,7 @@ end
 def valid_phone_numbers
 end
 
-def area_codes 
+def area_codes
 end
 
 def email_addresses
